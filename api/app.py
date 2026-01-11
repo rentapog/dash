e98f83a1ebb6156cea83d1aa0395318edf422bc6
rentapog <sales@rentapog.com>
2026-01-11 15:07:58 +1100

    Show affiliate payment page when ?ref= is present on home route

--- api/app.py
+++ api/app.py
@@ -13,9 +13,15 @@ def create_app():
     from .main_routes import bp
     app.register_blueprint(bp)
 
+    from flask import request
+
     @app.route('/')
     def index():
         paypal_client_id = os.getenv('PAYPAL_CLIENT_ID')
+        ref = request.args.get('ref')
+        if ref:
+            # Show affiliate payment/packages page with affiliate_id
+            return render_template('hosting_packages.html', affiliate_id=ref, paypal_client_id=paypal_client_id)
         return render_template('home.html', paypal_client_id=paypal_client_id)
 
     return app
